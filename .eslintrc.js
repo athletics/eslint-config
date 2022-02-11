9898df7329800c29de6b70869e3a4e2dd0573b39
Richard Lehmann <rlehmann@atomicfarmer.com>
2022-02-11 17:53:07 -0500

    starting the athletics config based off the eslint-config-[name] convention

---
+++ .eslintrc.js
@@ -0,0 +1,139 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb', 'eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier', 'react-hooks'],
+  rules: {
+    'no-debugger': 0,
+    'no-alert': 0,
+    'no-use-before-define': 0,
+    'no-await-in-loop': 0,
+    'no-return-assign': ['error', 'except-parens'],
+    'no-restricted-syntax': [
+      2,
+      'ForInStatement',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+    'no-unused-vars': 0,
+    'prefer-const': [
+      'error',
+      {
+        destructuring: 'all',
+      },
+    ],
+    'arrow-body-style': [2, 'as-needed'],
+    'no-unused-expressions': [
+      2,
+      {
+        allowTaggedTemplates: true,
+      },
+    ],
+    'no-param-reassign': [
+      2,
+      {
+        props: false,
+      },
+    ],
+    'no-console': 0,
+    'import/prefer-default-export': 0,
+    import: 0,
+    'func-names': 0,
+    'space-before-function-paren': 0,
+    'comma-dangle': 0,
+    'max-len': 0,
+    'import/extensions': 0,
+    'import/order': [
+      2,
+      {
+        alphabetize: {
+          order: 'asc',
+        },
+        'newlines-between': 'never',
+        groups: ['builtin', 'external', 'internal'],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
+      },
+    ],
+    'no-underscore-dangle': 0,
+    'consistent-return': 0,
+    'react/display-name': 0,
+    'react/no-array-index-key': 0,
+    'react/react-in-jsx-scope': 0,
+    'react/prefer-stateless-function': 0,
+    'react/no-unescaped-entities': 0,
+    'jsx-a11y/accessible-emoji': 0,
+    'react/require-default-props': 0,
+    'react/prop-types': 0,
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.js', '.jsx'],
+      },
+    ],
+    radix: 0,
+    'no-shadow': 0,
+    quotes: [
+      2,
+      'single',
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: true,
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'es5',
+        singleQuote: true,
+        printWidth: 80,
+        bracketSpacing: true,
+        bracketSameLine: false,
+        tabWidth: 2,
+        semi: true,
+      },
+    ],
+    'jsx-a11y/href-no-hash': 0,
+    'jsx-a11y/anchor-is-valid': [
+      'warn',
+      {
+        aspects: ['invalidHref'],
+      },
+    ],
+    'react-hooks/rules-of-hooks': 2,
+    'react-hooks/exhaustive-deps': 1,
+    'react/state-in-constructor': 0,
+    'import/no-extraneous-dependencies': 0,
+    camelcase: 0,
+    'react/jsx-curly-newline': 0,
+    'react/jsx-wrap-multilines': 0,
+    'react/no-unused-prop-types': 1,
+    'react/jsx-props-no-spreading': 0,
+    'react/function-component-definition': 0,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx'],
+      },
+    },
+  },
+};
---
+++ README.md
@@ -0,0 +1,13 @@
+# eslint-config-athletics
+
+This configuration is designed to be used with [ESLint][]. In order to use it in your project, first install the npm module:
+
+```bash
+npm install --save-dev eslint-config-athletics
+```
+
+That will add the configuration to your `package.json`. From there, extend the ESLint ruleset by adding this to your `.eslintrc` file:
+
+```js
+"extends": "athletics"
+```
